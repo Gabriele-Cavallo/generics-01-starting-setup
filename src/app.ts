b0fa697e9780f53daac8874a56901814e6f3963d
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-20 13:30:15 +0200

    generic

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-// Code goes here!
+// const names: string[] = ['Max', 'Manuel'];
+// const names: Array<string> = ['Max', 'Manu'];
+
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('This is done');
+//     }, 2000)
+// });
+
+// promise.then(data => {
+//     data.split(' ');
+// })
+
+function merge<T, U>(objA: T, objB: U){
+    return Object.assign({},objA, objB);
+}
+
+// console.log(merge({ name: 'Max' }, { age: 30 }));
+
+const mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+// const mergeObj = merge<{name: string, hobbies: string[]}, {age: number}>({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+// const mergeObj2 = merge({ name: 'Max' }, { age: 30 });
+console.log('age' , mergeObj.age);
