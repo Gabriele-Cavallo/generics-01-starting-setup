114a022265c99b423705647fd56744b9c7fb3634
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-20 14:20:03 +0200

    Partial + Readonly

--- src/app.ts
+++ src/app.ts
@@ -79,3 +79,21 @@ console.log(numberStorage.getItems());
 // objStorage.addItem({name: 'Manu'});
 // objStorage.removeItem(maxObj);
 // console.log(objStorage.getItems());
+
+interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+}
+
+function createCourseGoal(title: string, description: string, date: Date){
+    // return {title: title, description: description, completeUntil: date};
+    let courseGoal: Partial<CourseGoal> = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    return courseGoal as CourseGoal;
+};
+
+const names: Readonly<string[]> = ['Max', 'Anna'];
+// names.push('Manu');
