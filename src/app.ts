5a37340ac94b2ac06974770f34d44f3427ec3a3f
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-20 13:50:15 +0200

    keyof constraint

--- src/app.ts
+++ src/app.ts
@@ -21,3 +21,26 @@ const mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 // const mergeObj = merge<{name: string, hobbies: string[]}, {age: number}>({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 // const mergeObj2 = merge({ name: 'Max' }, { age: 30 });
 console.log('age' , mergeObj.age);
+
+
+interface Lengthy{
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string]{
+    let descriptionText = 'Got no value.';
+    if(element.length === 1){
+        descriptionText = 'Got 1 element';
+    }else if(element.length > 1){
+        descriptionText = 'Got ' + element.length + ' elements.';
+    }
+    return [element, descriptionText];
+};
+
+console.log( countAndDescribe('Hi there!'));
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U){
+    return 'Value: ' + obj[key];
+};
+
+extractAndConvert({name: 'Max'}, 'name')
